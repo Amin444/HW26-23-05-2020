3e42b86c4688ed3949f990898f906774c3bb0fb0
Aminjon <aminmirzoev123@gmail.com>
2020-05-25 21:47:30 +0500

    Добавьте файлы проекта.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HW26__23_05_2020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
